469b8a7ceb8c186cebe6a678da67f8dde048950e
Matija Bruncic <matija.bruncic@tink.com>
2022-02-03 21:41:51 +0100

    Version 2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### About
 Chrome extension that tracks how much time you spend on Google Meet
+
+### Issues
+- \<if you find an issue, add it here>
--- background.js
+++ background.js
@@ -1,130 +1,154 @@
-const activeTabs = {};
-const tabHistory = [];
 const verbose = false
+const storage = {};
 
-chrome.action.setIcon({path:"images/default-38.png"})
+class Period {
+    start
+    end
+
+    constructor() {
+        this.start = new Date()
+        this.end = new Date()
+    }
+
+    prolong() {
+        this.end = new Date()
+    }
+
+    timeSpent() {
+        return this.end - this.start
+    }
+
+}
+
+class DayPeriods {
+
+    periods = []
+    existingPeriod
+
+    endPeriod() {
+        this.existingPeriod.prolong()
+        this.periods.push(this.existingPeriod)
+        this.existingPeriod = null
+    }
+
+    prolong() {
+        if (this.existingPeriod == null) {
+            throw "Not possible to prolong the period when existing period == null"
+        }
+        this.existingPeriod.prolong()
+    }
+
+    startNewPeriod() {
+        this.existingPeriod = new Period()
+        return this.existingPeriod
+    }
+
+    getExistingPeriod() {
+        if (this.existingPeriod == null) {
+            return null
+        }
+        return this.existingPeriod;
+    }
+
+    getCumulative() {
+        let result = 0
+        for (const period of this.periods) {
+            result += period.timeSpent()
+        }
+        if (this.existingPeriod != null) {
+            result += this.existingPeriod.timeSpent()
+        }
+        return result
+    }
+}
+
+chrome.action.setIcon({path: "images/default-38.png"})
+
 chrome.tabs.onCreated.addListener((tab) => {
-        if (verbose) {
-            console.log("OnCreated event received: " + JSON.stringify(tab))
-        }
-        startGoogleMeet(tab);
-    }
-)
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-        if (verbose) {
-            console.log("OnUpdated event received: " + JSON.stringify(tab))
-        }
-        if (movingAwayFromGoogleMeet(tab)) {
-            console.log("Moving from Google Meet to some other page")
-            stopGoogleMeet(tab.id)
-        }
-        startGoogleMeet(tab)
-    }
-)
+    validate()
+})
+chrome.tabs.onUpdated.addListener((tab) => {
+    validate()
+})
 chrome.tabs.onRemoved.addListener((tabId, _) => {
-        if (verbose) {
-            console.log("OnRemoved event received: " + tabId)
-        }
-        stopGoogleMeet(tabId);
-    }
-)
+    validate()
+})
 
 chrome.alarms.onAlarm.addListener((alarm) => {
         if (verbose) {
             console.log("OnAlarm event received: " + JSON.stringify(alarm))
         }
-        if (alarm.name === "update-counter") {
-            refreshTimeSpent()
-        } else if (alarm.name === "validation-checker") {
-            validate()
+        if (alarm.name === "refresh-icon") {
+            refreshIcon()
+        } else if (alarm.name === "tracker") {
+            tracker()
+        } else {
+            throw "Alarm is missing. Alarm name:" + alarm.name
         }
     }
 )
-chrome.alarms.create("update-counter", {periodInMinutes: 0.1})
-chrome.alarms.create("validation-checker", {periodInMinutes: 0.1})
-
-function cleanupUrl(tab) {
-    return tab.url.replace(/\?.*/, "");
-}
+chrome.alarms.create("refresh-icon", {periodInMinutes: 0.01})
+chrome.alarms.create("tracker", {periodInMinutes: 0.01})
 
 function isOnGoogleMeet(tab) {
     return /https:\/\/meet.google.com\/.*/i.test(tab.url);
 }
 
-function startGoogleMeet(tab) {
-    if (!isOnGoogleMeet(tab)) {
-        return
-    }
-    if (verbose){
-        console.log("Opening Google Meet")
-    }
-    let status = activeTabs[tab.id]
-    if (status == null) {
-        status = {}
-    }
-    status["url"] = cleanupUrl(tab)
-    if (!("created" in status)) {
-        status["created"] = new Date()
-    }
-    activeTabs[tab.id] = status
-    validate()
-}
-
-function movingAwayFromGoogleMeet(tab) {
-    if (isOnGoogleMeet(tab)) {
-        return false;
-    }
-    if (activeTabs[tab.id] === undefined) {
-        return false;
-    }
-    if (!isOnGoogleMeet(activeTabs[tab.id])) {
-        return false;
-    }
-    return true;
-}
-
-function refreshTimeSpent() {
+function refreshIcon() {
     let totalTimeInMs = 0
-    for (const tabHistoryElement of tabHistory) {
-        let diff = tabHistoryElement.removed - tabHistoryElement.created
-        totalTimeInMs += diff
-    }
-    for (const property in activeTabs) {
-        let status = activeTabs[property]
-        let diff = new Date() - status.created
-        totalTimeInMs += diff
-        if (verbose){
-            console.log("Diff for active tab is: " + diff + " ms")
-        }
-    }
-
-    if (verbose){
-        console.log("Active tabs: " + JSON.stringify(activeTabs, null, 4))
-        console.log("Tab history: " + JSON.stringify(tabHistory, null, 4))
-        console.log("Total time in ms: " + totalTimeInMs)
-    }
-    let hours = Math.round(totalTimeInMs / 3600000)
-    let minutes = Math.round((totalTimeInMs - (hours * 3600000)) / 60000)
+    let todaysPeriods = storage[getTodaysDate()];
+    totalTimeInMs = todaysPeriods == null ? 0 : todaysPeriods.getCumulative();
+    let hours = Math.floor(totalTimeInMs / 3600000)
+    let minutes = Math.floor((totalTimeInMs - (hours * 3600000)) / 60000)
+    let seconds = Math.floor((totalTimeInMs - (hours * 3600000) - (minutes * 60000)) / 1000)
 
     if (hours < 2) {
-        chrome.action.setBadgeBackgroundColor(
-            {color: "green"}
-        );
+        chrome.action.setBadgeBackgroundColor({color: "green"});
     } else if (hours < 4) {
-        chrome.action.setBadgeBackgroundColor(
-            {color: 'orange'}
-        )
+        chrome.action.setBadgeBackgroundColor({color: 'orange'});
     } else {
-        chrome.action.setBadgeBackgroundColor(
-            {color: 'red'}
-        )
+        chrome.action.setBadgeBackgroundColor({color: 'red'});
     }
 
-    let text = String(hours) + "h" + String(minutes) + "m";
+    let text = hours < 1 ? String(minutes) + "m" + String(seconds) + "s" : String(hours) + "h" + String(minutes) + "m";
     if (verbose) {
         console.log(text)
     }
     chrome.action.setBadgeText({text: text});
+}
+
+function getTodaysDate() {
+    return new Date().toISOString().split('T')[0];
+}
+
+function tracker() {
+    chrome.tabs.query({}, function (tabs) {
+        let googleMeetCounter = 0
+        for (const tab of tabs) {
+            if (isOnGoogleMeet(tab)) {
+                googleMeetCounter++
+            }
+        }
+        let date = getTodaysDate();
+        let todaysPeriods = storage[date] != null ? storage[date] : new DayPeriods();
+        let existingPeriod = todaysPeriods.getExistingPeriod()
+        if (existingPeriod == null && googleMeetCounter > 0) { //beginning of new period
+            todaysPeriods.startNewPeriod()
+        } else if (existingPeriod != null && googleMeetCounter > 0) { //continuation of existing period
+            existingPeriod.prolong()
+        } else if (existingPeriod != null && googleMeetCounter === 0) { //end of a period
+            todaysPeriods.endPeriod()
+        } else if (existingPeriod == null && googleMeetCounter === 0) { //Google Meet is not opened
+            //don't do anything
+        } else {
+            throw "Something is wrong - debug!"
+        }
+        storage[date] = todaysPeriods
+
+        if (verbose) {
+            console.log(todaysPeriods)
+        }
+    })
 }
 
 function validate() {
@@ -135,27 +159,15 @@ function validate() {
                 googleMeetCounter++
             }
         }
-        if (verbose){
+        if (verbose) {
             console.log(googleMeetCounter + "/" + tabs.length + " tabs are using Google Meet")
         }
-        if (googleMeetCounter>1){
-            chrome.action.setIcon({path:"images/error-38.png"})
+        if (googleMeetCounter > 1) {
+            chrome.action.setIcon({path: "images/error-38.png"})
+        } else if (googleMeetCounter === 0 || storage[getTodaysDate()] == null || storage[getTodaysDate()].getExistingPeriod() == null) {
+            chrome.action.setIcon({path: "images/gray-38.png"})
         } else {
-            chrome.action.setIcon({path:"images/up-38.png"})
+            chrome.action.setIcon({path: "images/up-38.png"})
         }
     });
-}
-
-function stopGoogleMeet(tabId) {
-    let status = activeTabs[tabId]
-    if (status === undefined) {
-        return
-    }
-    if (!("removed" in status)) {
-        status["removed"] = new Date()
-    }
-    tabHistory.push(status)
-    delete activeTabs[tabId]
-    refreshTimeSpent()
-    validate()
 }
