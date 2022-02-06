a7c87cd45d9e681f92973ca6ccb7d52a8129bf23
Matija Bruncic <matija.bruncic@tink.com>
2022-02-06 17:18:01 +0100

    feat(storage): use local storage instead of variable to prevent data loss

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 const verbose = false
-const storage = {};
 
 class Period {
     start
@@ -9,6 +8,17 @@ class Period {
         this.start = new Date()
         this.end = new Date()
     }
+
+    static fromJSON(old) {
+        if (old == null) {
+            return null
+        }
+        let newObject = new Period();
+        newObject.start = old.start != null ? new Date(old.start) : null
+        newObject.end = old.end != null ? new Date(old.end) : null
+        return newObject
+    }
+
 
     prolong() {
         this.end = new Date()
@@ -24,6 +34,18 @@ class DayPeriods {
 
     periods = []
     existingPeriod
+
+    static fromJSON(old) {
+        if (old == null) {
+            return null
+        }
+        let newObject = new DayPeriods();
+        newObject.existingPeriod = Period.fromJSON(old.existingPeriod)
+        for (const period of old.periods) {
+            newObject.periods.push(Period.fromJSON(period))
+        }
+        return newObject
+    }
 
     endPeriod() {
         this.existingPeriod.prolong()
@@ -87,8 +109,8 @@ chrome.alarms.onAlarm.addListener((alarm) => {
         }
     }
 )
-chrome.alarms.create("refresh-icon", {periodInMinutes: 0.01})
-chrome.alarms.create("tracker", {periodInMinutes: 0.01})
+chrome.alarms.create("refresh-icon", {periodInMinutes: 0.015})
+chrome.alarms.create("tracker", {periodInMinutes: 0.015})
 
 function isOnGoogleMeet(tab) {
     return /https:\/\/meet.google.com\/.*/i.test(tab.url);
@@ -96,25 +118,27 @@ function isOnGoogleMeet(tab) {
 
 function refreshIcon() {
     let totalTimeInMs = 0
-    let todaysPeriods = storage[getTodaysDate()];
-    totalTimeInMs = todaysPeriods == null ? 0 : todaysPeriods.getCumulative();
-    let hours = Math.floor(totalTimeInMs / 3600000)
-    let minutes = Math.floor((totalTimeInMs - (hours * 3600000)) / 60000)
-    let seconds = Math.floor((totalTimeInMs - (hours * 3600000) - (minutes * 60000)) / 1000)
+    chrome.storage.local.get(getTodaysDate(), function (days) {
+        let todaysPeriods = DayPeriods.fromJSON(JSON.parse(days[getTodaysDate()]));
+        totalTimeInMs = days[getTodaysDate()] == null ? 0 : todaysPeriods.getCumulative();
+        let hours = Math.floor(totalTimeInMs / 3600000)
+        let minutes = Math.floor((totalTimeInMs - (hours * 3600000)) / 60000)
+        let seconds = Math.floor((totalTimeInMs - (hours * 3600000) - (minutes * 60000)) / 1000)
 
-    if (hours < 2) {
-        chrome.action.setBadgeBackgroundColor({color: "green"});
-    } else if (hours < 4) {
-        chrome.action.setBadgeBackgroundColor({color: 'orange'});
-    } else {
-        chrome.action.setBadgeBackgroundColor({color: 'red'});
-    }
+        if (hours < 2) {
+            chrome.action.setBadgeBackgroundColor({color: "green"});
+        } else if (hours < 4) {
+            chrome.action.setBadgeBackgroundColor({color: 'orange'});
+        } else {
+            chrome.action.setBadgeBackgroundColor({color: 'red'});
+        }
 
-    let text = hours < 1 ? String(minutes) + "m" + String(seconds) + "s" : String(hours) + "h" + String(minutes) + "m";
-    if (verbose) {
-        console.log(text)
-    }
-    chrome.action.setBadgeText({text: text});
+        let text = hours < 1 ? String(minutes) + "m" + String(seconds) + "s" : String(hours) + "h" + String(minutes) + "m";
+        if (verbose) {
+            console.log(text)
+        }
+        chrome.action.setBadgeText({text: text});
+    });
 }
 
 function getTodaysDate() {
@@ -130,24 +154,31 @@ function tracker() {
             }
         }
         let date = getTodaysDate();
-        let todaysPeriods = storage[date] != null ? storage[date] : new DayPeriods();
-        let existingPeriod = todaysPeriods.getExistingPeriod()
-        if (existingPeriod == null && googleMeetCounter > 0) { //beginning of new period
-            todaysPeriods.startNewPeriod()
-        } else if (existingPeriod != null && googleMeetCounter > 0) { //continuation of existing period
-            existingPeriod.prolong()
-        } else if (existingPeriod != null && googleMeetCounter === 0) { //end of a period
-            todaysPeriods.endPeriod()
-        } else if (existingPeriod == null && googleMeetCounter === 0) { //Google Meet is not opened
-            //don't do anything
-        } else {
-            throw "Something is wrong - debug!"
-        }
-        storage[date] = todaysPeriods
-
-        if (verbose) {
-            console.log(todaysPeriods)
-        }
+        chrome.storage.local.get(date, function (days) {
+            let todaysPeriods = days[date] != null ? DayPeriods.fromJSON(JSON.parse(days[date])) : new DayPeriods();
+            let existingPeriod = todaysPeriods.getExistingPeriod()
+            if (existingPeriod == null && googleMeetCounter > 0) { //beginning of new period
+                todaysPeriods.startNewPeriod()
+            } else if (existingPeriod != null && googleMeetCounter > 0) { //continuation of existing period
+                existingPeriod.prolong()
+            } else if (existingPeriod != null && googleMeetCounter === 0) { //end of a period
+                todaysPeriods.endPeriod()
+            } else if (existingPeriod == null && googleMeetCounter === 0) { //Google Meet is not opened
+                //don't do anything
+                if (verbose) {
+                    console.log("Google Meet is not open")
+                }
+            } else {
+                throw "Something is wrong - debug!"
+            }
+            chrome.storage.local.set({[date]: JSON.stringify(todaysPeriods)}, function () {
+                if (verbose) {
+                    chrome.storage.local.get(getTodaysDate(), function (days) {
+                        console.log(`Saved ${date} to storage: ${JSON.stringify(days)}. Todays periods: ${JSON.stringify(todaysPeriods)}`)
+                    })
+                }
+            });
+        })
     })
 }
 
@@ -162,12 +193,14 @@ function validate() {
         if (verbose) {
             console.log(googleMeetCounter + "/" + tabs.length + " tabs are using Google Meet")
         }
-        if (googleMeetCounter > 1) {
-            chrome.action.setIcon({path: "images/error-38.png"})
-        } else if (googleMeetCounter === 0 || storage[getTodaysDate()] == null || storage[getTodaysDate()].getExistingPeriod() == null) {
-            chrome.action.setIcon({path: "images/gray-38.png"})
-        } else {
-            chrome.action.setIcon({path: "images/up-38.png"})
-        }
+        chrome.storage.local.get(getTodaysDate(), function (days) {
+            if (googleMeetCounter > 1) {
+                chrome.action.setIcon({path: "images/error-38.png"})
+            } else if (googleMeetCounter === 0 || days[getTodaysDate()] == null || DayPeriods.fromJSON(JSON.parse(days[getTodaysDate()])).getExistingPeriod() == null) {
+                chrome.action.setIcon({path: "images/gray-38.png"})
+            } else {
+                chrome.action.setIcon({path: "images/up-38.png"})
+            }
+        })
     });
 }
